9d19d6861bdcf75ad607e5e150594af5bf3876bb
kdh5107 <bjmb12@naver.com>
2023-01-22 19:21:35 +0900

    JS : home transparent

--- main.js
+++ main.js
@@ -28,6 +28,13 @@ contactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+// make home slowly fade to transparent as the window scolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
